08d715306179a56558aa103eb36346450e774d01
Z4LMT <dungnguyen10082000@gmail.com>
2023-10-15 14:52:52 +1100

    Update autostart.sh

--- qtile/autostart.sh
+++ qtile/autostart.sh
@@ -2,18 +2,6 @@
 
 COLORSCHEME=Catppuccin
 
-### CHECKS IF VIRTUAL MACHINE ###
-# If so, this sets an appropriate screen resolution.
-# This is needed as part of DTOS.
-if [[ $(systemd-detect-virt) = "none" ]]; then
-    echo "Not running in a Virtual Machine";
-elif xrandr | grep "1366x768"; then
-    xrandr -s 1366x768 || echo "Cannot set 1366x768 resolution.";
-elif xrandr | grep "1920x1080"; then
-    xrandr -s 1920x1080 || echo "Cannot set 1920x1080 resolution.";
-else echo "Could not set a resolution."
-fi
-
 
 ### AUTOSTART PROGRAMS ###
 lxsession &
