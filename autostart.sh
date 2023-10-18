6d87026ac914c5e9169b43167a9e619a0c4a6ce3
Z4LMT <dungnguyen10082000@gmail.com>
2023-10-18 19:26:11 +1100

    Add files via upload

---
+++ autostart.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash 
+
+COLORSCHEME=Catppuccin
+
+
+### AUTOSTART PROGRAMS ###
+lxsession &
+# picom &
+copyq &
+nm-applet &
+pamac-tray-icon-plasma &
+# birdtray &
+# cbatticon &
+xss-lock -- i3lock -ei ~/.config/qtile/lockscreen.png &
+# xss-lock -- i3lock -B 10 &
+"$HOME"/.screenlayout/layout.sh &
+
+### UNCOMMENT ONLY ONE OF THE FOLLOWING THREE OPTIONS! ###
+# 1. Uncomment to restore last saved wallpaper
+# xargs xwallpaper --stretch < ~/.cache/wall &
+# 2. Uncomment to set a random wallpaper on login
+# find /usr/share/backgrounds/dtos-backgrounds/ -type f | shuf -n 1 | xargs xwallpaper --stretch &
+# 3. Uncomment to set wallpaper with nitrogen
+nitrogen --restore &
+
